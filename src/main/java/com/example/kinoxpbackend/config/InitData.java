ccd04bcce478d9446d42f79bad4c164627513ce6
Chrilleweb <christian.munknissen@gmail.com>
2023-10-02 23:15:18 +0200

    test data

--- src/main/java/com/example/kinoxpbackend/config/InitData.java
+++ src/main/java/com/example/kinoxpbackend/config/InitData.java
@@ -33,7 +33,7 @@ public class InitData implements CommandLineRunner {
         movie2.setImageUrl("https://img-cdn.sfanytime.com/COVERM/99a66254-3e74-4698-b9fb-9f81010f5574_COVERM_01.jpg?w=375&ar=0.692&fit=crop&fm=pjpg&s=9b39d41ba54811a9f2ba609ee8d780dd");
         movie2.setCategory(MovieCategories.COMEDY);
         movie2.setAgeLimit(AgeLimit.PG_13);
-        movie2.setDuration(110.5);
+        movie2.setDuration(110.0);
         movie2.setDescription("Another movie description");
 
         // Save movies to the database
