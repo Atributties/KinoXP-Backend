3abf7adba26cf4e25a65fd174896beb21a1d57e7
marcusFN <marc599n@stud.kea.dk>
2023-10-03 14:04:28 +0200

    added ekstra movie test data

--- src/main/java/com/example/kinoxpbackend/config/InitData.java
+++ src/main/java/com/example/kinoxpbackend/config/InitData.java
@@ -37,18 +37,17 @@ public class InitData implements CommandLineRunner {
         movie2.setDescription("Another movie description");
 
         Movie movie3 = new Movie();
-        movie2.setTitle("Movie title 3");
-        movie2.setImageUrl("https://img-cdn.sfanytime.com/COVERM/99a66254-3e74-4698-b9fb-9f81010f5574_COVERM_01.jpg?w=375&ar=0.692&fit=crop&fm=pjpg&s=9b39d41ba54811a9f2ba609ee8d780dd");
-        movie2.setCategory(MovieCategories.DOCUMENTARY);
-        movie2.setAgeLimit(AgeLimit.PG_13);
-        movie2.setDuration(190.0);
-        movie2.setDescription("Another movie description");
+        movie3.setTitle("Movie title 3");
+        movie3.setImageUrl("https://img-cdn.sfanytime.com/COVERM/99a66254-3e74-4698-b9fb-9f81010f5574_COVERM_01.jpg?w=375&ar=0.692&fit=crop&fm=pjpg&s=9b39d41ba54811a9f2ba609ee8d780dd");
+        movie3.setCategory(MovieCategories.DOCUMENTARY);
+        movie3.setAgeLimit(AgeLimit.PG_13);
+        movie3.setDuration(190.0);
+        movie3.setDescription("Another movie description");
 
         // Save movies to the database
         movieService.save(movie1);
         movieService.save(movie2);
-
-
+        movieService.save(movie3);
 
 
 
