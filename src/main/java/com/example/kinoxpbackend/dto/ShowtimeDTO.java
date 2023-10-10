8906a6d9eb2482d546d9a45abb350e59038d90ed
MT <113047767+Mathias-Thomsen@users.noreply.github.com>
2023-10-10 16:33:10 +0200

    Viser theater i reservation i JSON objekt.

--- src/main/java/com/example/kinoxpbackend/dto/ShowtimeDTO.java
+++ src/main/java/com/example/kinoxpbackend/dto/ShowtimeDTO.java
@@ -1,6 +1,7 @@
 package com.example.kinoxpbackend.dto;
 
 import com.example.kinoxpbackend.entities.Showtime;
+import com.example.kinoxpbackend.entities.Theater;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -9,12 +10,14 @@ public class ShowtimeDTO {
     private int id;
     private LocalDate date;
     private LocalTime time;
+    private String theaterName ;
 
 
     public ShowtimeDTO(Showtime showtime) {
         this.id = showtime.getId();
         this.date = showtime.getDate();
         this.time = showtime.getTime();
+        this.theaterName  = String.valueOf(showtime.getTheater().getTheaterName());
     }
 
     public int getId() {
@@ -40,5 +43,15 @@ public class ShowtimeDTO {
 
     public void setTime(LocalTime time) {
         this.time = time;
+    }
+
+    // Getter for teaternavnet
+    public String getTheaterName() {
+        return theaterName;
+    }
+
+    // Setter for teaternavnet, hvis det er relevant
+    public void setTheaterName(String theaterName) {
+        this.theaterName = theaterName;
     }
 }
